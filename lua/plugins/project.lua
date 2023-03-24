c8afdbc1570e882d3da1671d1055315cd792d9db
CptCaptain <nilskoch@posteo.de>
2023-03-24 12:35:57 +0100

    ignore term for nvim-tree.chdir, causes errors on Lazygit and similar otherwise

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -14,7 +14,8 @@ return {
       respect_buf_cwd = true,
       update_focused_file = {
         enable = true,
-        update_root = true
+        update_root = true,
+        ignore_list = { "toggleterm", "term" },
       },
     })
   end
