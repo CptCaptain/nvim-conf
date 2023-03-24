d4b691f02143933933bd45fa8a4bc8f701eaefa7
CptCaptain <nilskoch@posteo.de>
2023-03-24 17:33:22 +0100

    map localleader at start of init, otherwise it doesn't work...

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
+vim.g.maplocalleader = ","
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.g.mapleader = " "
-vim.g.maplocalleader = ","
 
 
 require("plugins")
