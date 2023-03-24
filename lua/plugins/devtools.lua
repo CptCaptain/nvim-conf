04a9cf3c07064f29cf06126a732ebedfb0ce022d
CptCaptain <nilskoch@posteo.de>
2023-03-24 13:46:13 +0100

    try to make autocomplete somewhat better

--- lua/plugins/devtools.lua
+++ lua/plugins/devtools.lua
@@ -134,13 +134,20 @@ return {
     },
     opts = function()
       local cmp = require("cmp")
+      local ls = require("luasnip")
+      local has_words_before = function ()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
       return {
         completion = {
-          completeopt = "menu,menuone,noinsert",
+          completeopt = "menu,menuone,noinsert,noselect,preview",
         },
         snippet = {
           expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            ls.lsp_expand(args.body)
           end,
         },
         mapping = cmp.mapping.preset.insert({
@@ -148,7 +155,27 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<Tab>"] = cmp.mapping(function (fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif ls.expand_or_locally_jumpable() then
+              ls.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function (fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+              elseif ls.jumpable(-1) then
+              ls.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
